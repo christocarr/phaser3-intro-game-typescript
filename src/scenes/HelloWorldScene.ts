dc307f2439af7ea21162fb39fc34dc5f5bc73aa5
christocarr <christocarr@outlook.com>
2020-05-18 12:50:18 +0100

    Initial commit

---
+++ src/scenes/HelloWorldScene.ts
@@ -0,0 +1,23 @@
+import Phaser from 'phaser'
+
+export default class HelloWorldScene extends Phaser.Scene
+{
+	constructor()
+	{
+		super('hello-world')
+	}
+
+	preload()
+    {
+
+    }
+
+    create()
+    {
+
+    }
+
+    update() {
+        
+    }
+}
