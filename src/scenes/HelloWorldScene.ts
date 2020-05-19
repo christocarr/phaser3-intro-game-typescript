db7da0fa88c9cf2fb436c651598d41778cbb00e9
christocarr <christocarr@outlook.com>
2020-05-19 12:56:37 +0100

    Add player character

--- src/scenes/HelloWorldScene.ts
+++ src/scenes/HelloWorldScene.ts
@@ -3,6 +3,8 @@ import Phaser from 'phaser'
 export default class HelloWorldScene extends Phaser.Scene
 {
     private platforms?: Phaser.Physics.Arcade.StaticGroup
+    private player?: Phaser.Physics.Arcade.Sprite
+
 	constructor()
 	{
 		super('hello-world')
@@ -29,6 +31,34 @@ export default class HelloWorldScene extends Phaser.Scene
         this.platforms.create(600, 400, 'ground')
         this.platforms.create(50, 250, 'ground')
         this.platforms.create(750, 220, 'ground')
+
+        this.player = this.physics.add.sprite(100, 450, 'dude')
+        this.player.setBounce(0.2)
+        this.player.setCollideWorldBounds(true)
+
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('dude', {
+                start: 0, end: 3
+            }),
+            frameRate: 10,
+            repeat: -1
+        })
+
+        this.anims.create({
+            key: ' turn',
+            frames: [{ key: 'dude', frame: 4 }],
+            frameRate: 20
+        })
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('dude', {
+                start: 5, end: 8
+            }),
+            frameRate: 10,
+            repeat: -1
+        })
     }
 
     update() {
