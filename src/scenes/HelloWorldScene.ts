3ec7e3f94276528a656d72e0d0f42743dd49cba5
christocarr <christocarr@outlook.com>
2020-05-19 12:16:12 +0100

    Add ground

--- src/scenes/HelloWorldScene.ts
+++ src/scenes/HelloWorldScene.ts
@@ -2,6 +2,7 @@ import Phaser from 'phaser'
 
 export default class HelloWorldScene extends Phaser.Scene
 {
+    private platforms?: Phaser.Physics.Arcade.StaticGroup
 	constructor()
 	{
 		super('hello-world')
@@ -9,15 +10,24 @@ export default class HelloWorldScene extends Phaser.Scene
 
 	preload()
     {
-
+        this.load.image('sky', 'assets/sky.png')
+        this.load.image('ground', 'assets/platform.png')
+        this.load.image('star', 'assets/star.png')
+        this.load.image('bomb', 'assets/bomb.png')
+        this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 }))
     }
 
     create()
     {
-
+        this.add.image(400, 300, 'sky')
+        this.platforms = this.physics.add.staticGroup()
+        const ground = this.platforms.create(400, 568, 'ground') as Phaser.Physics.Arcade.Sprite
+            ground
+                .setScale(2)
+                .refreshBody()
     }
 
     update() {
-        
+
     }
 }
